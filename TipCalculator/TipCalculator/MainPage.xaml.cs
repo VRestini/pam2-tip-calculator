3a637216ea7f8675f1840b573d95fa5f059ab6c2
Vitor e Rafael <vitorsouzalibrelonrestini@gmail.com>
2024-03-26 11:15:26 -0300

    a

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentageLabel.Text = $"{PercentageSlider.Value.ToString()}%";
+        }
+
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button30Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
